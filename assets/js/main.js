9c59d53b8ed47f32922c2e47e34753dfd94f343d
Fadli Jaya <jaya.fadly@gmail.com>
2024-01-05 00:48:16 +0800

    add certificate

--- assets/js/main.js
+++ assets/js/main.js
@@ -213,6 +213,45 @@
     }
   });
 
+   /**
+   * Certificate isotope and filter
+   */
+   window.addEventListener('load', () => {
+    let certificateContainer = select('.certificate-container');
+    if (certificateContainer) {
+      let certificateIsotope = new Isotope(certificateContainer, {
+        itemSelector: '.certificate-item'
+      });
+
+      let certificateFilters = select('#certificate-flters li', true);
+
+      on('click', '#certificate-flters li', function(e) {
+        e.preventDefault();
+        certificateFilters.forEach(function(el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
+
+        certificateIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+        certificateIsotope.on('arrangeComplete', function() {
+          AOS.refresh()
+        });
+      }, true);
+    }
+
+  });
+
+
+  /**
+   * Initiate certificate lightbox 
+   */
+  const certificateLightbox = GLightbox({
+    selector: '.certificate-lightbox'
+  });
+
+
   /**
    * Testimonials slider
    */
